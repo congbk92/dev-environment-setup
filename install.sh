84edc78d5a3d9ef161964ca7073d7d1564364797
thanhcong <thanhcong.bk10@gmail.com>
2025-04-25 19:16:07 +0700

    Update

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@
 
 echo "Running all install.sh scripts..."
 
-find . -mindepth 2 -name "install.sh" -exec bash -c '
-    dir="$(dirname "{}")"
-    echo "Running install.sh in $dir"
-    (cd "$dir" && ./install.sh)
-' \;
-
+# find . -mindepth 2 -name "install.sh" -exec bash -c '
+#     dir="$(dirname "{}")"
+#     echo "Running install.sh in $dir"
+#     (cd "$dir" && ./install.sh)
+# ' \;
+#
 echo "Installations complete."
 echo "Don't forget to update ~/.zshrc by: "
 echo "source ${PWD}/bashrc.sh"
-echo " eval \"$(fzf --zsh)\""
+echo "eval \"\$(fzf --zsh)\""
