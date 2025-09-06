4af477ac6bbd3096544155347c8b6b434470c715
Cong <thanhcong.bk10@gmail.com>
2025-09-06 22:35:08 +0700

    fix nvim install, update readme

--- README.md
+++ README.md
@@ -5,11 +5,15 @@ Notes: It has been tested in Unbuntu 24.04 LTS and 22.04 LTS only.
 
 ## Installation
 1. [Install nerd font](https://github.com/romkatv/powerlevel10k?tab=readme-ov-file#meslo-nerd-font-patched-for-powerlevel10k)
-2. Clone the repository and run the installation script:
+2. Install zsh.
+```sh
+sudo apt install zsh
+```
+3. Clone the repository and run the installation script:
 ```sh
 git clone https://github.com/yourusername/dev-environment-setup.git
 cd dev-environment-setup
 ./install.sh
 ```
-3. Update ~/.zshrc as the ouput of the previous step.
-4. `source ~/.zshrc` and enter prompt to config.
+4. Update ~/.zshrc as the ouput of the previous step.
+5. `source ~/.zshrc` and enter prompt to config.
--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NVIM_VERSION="0.10.3"
+NVIM_VERSION="0.11.4"
 INSTALL_DIR="$HOME/.local/nvim"
 BIN_DIR="$HOME/.local/bin"
 
@@ -8,7 +8,7 @@ echo "Installing Neovim to $INSTALL_DIR..."
 
 # Download and extract Neovim
 mkdir -p "$INSTALL_DIR"
-curl -L "https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux64.tar.gz" | tar -xz -C "$INSTALL_DIR" --strip-components=1 || { echo "failed to install nvim ${NVIM_VERSION}"; exit 1; }
+curl -L "https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux-x86_64.tar.gz" | tar -xz -C "$INSTALL_DIR" --strip-components=1 || { echo "failed to install nvim ${NVIM_VERSION}"; exit 1; }
 
 # Link binary
 ln -sf "$INSTALL_DIR/bin/nvim" "$BIN_DIR/nvim"
@@ -16,7 +16,7 @@ ln -sf "$INSTALL_DIR/bin/nvim" "$BIN_DIR/nvim"
 # Symlink configuration files
 CUR_DIR=$(pwd)
 mkdir -p ~/.config
-ln -sf $CUR_DIR/nvim/config ~/.config/nvim
+ln -sf $CUR_DIR/config ~/.config/nvim
 
 
 echo "Neovim installed successfully!"
